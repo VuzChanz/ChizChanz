7edca33b0ee030dc38381f02250cbf2ce45f7cca
VuzChanz <vuzchan.vn205@gmail.com>
2026-05-09 14:30:31 +0700

    Update VuzChanzUi.lua

--- Utils/VuzChanzUi.lua
+++ Utils/VuzChanzUi.lua
@@ -2182,7 +2182,7 @@ function VuzChanzUI:Window(Args)
                 BorderSizePixel = 0,
                 Position = UDim2.new(0.5, 0, 0.5, 0),
                 Size = UDim2.new(0.5, 0, 0.5, 0),
-                Image = "rbxassetid://134528790539968"
+                Image = "rbxassetid://129736089317081"
             })
 
             VuzChanzUI:Draggable(Pillow_1)
